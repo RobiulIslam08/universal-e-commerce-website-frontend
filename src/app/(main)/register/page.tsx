9da52d4da1810f55a6179b91648d9c1338a4f50b
Rabiul Islam <robiulislamrobi0874@gmail.com>
2025-12-19 16:40:43 +0600

    google login implementation

--- src/app/(main)/register/page.tsx
+++ src/app/(main)/register/page.tsx
@@ -418,7 +418,7 @@ export default function RegisterPage() {
 
           {/* Social signup */}
        <motion.div
-            className="grid grid-cols-2 gap-3"
+            className="grid grid-cols-1 gap-3"
             variants={itemVariants}
           >
             <Button
@@ -447,7 +447,7 @@ export default function RegisterPage() {
               </svg>
               <span className="text-xs font-medium">Google</span>
             </Button>
-            <Button
+            {/* <Button
               type="button"
               variant="outline"
               className="border-gray-200 hover:bg-gray-50"
@@ -456,7 +456,7 @@ export default function RegisterPage() {
                 <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
               </svg>
               <span className="text-xs font-medium">Facebook</span>
-            </Button>
+            </Button>  */}
           </motion.div>
         </motion.div>
 
